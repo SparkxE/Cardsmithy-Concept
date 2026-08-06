2886aab9ac3ce4493a94cb768e087370141bb088
Aaron Anderson <sylverblade16@gmail.com>
2026-08-06 14:58:35 -0500

    Fixed duplicate spawn location bug by making random tileID generation recursive instead of the entire spawn function

--- Assets/Combat System/Scripts/EnemySpawner.cs
+++ Assets/Combat System/Scripts/EnemySpawner.cs
@@ -6,7 +6,6 @@ using UnityEngine.Tilemaps;
 public class EnemySpawner : MonoBehaviour
 {
     private const int maxEnemies = 3;
-    private int indexCount;
     private int[] tileHasMob = new int[maxEnemies] { 0, 0, 0 };
     [SerializeField] private GameObject[] enemyList = new GameObject[maxEnemies];
     [SerializeField] private Transform startPosition;
@@ -14,36 +13,35 @@ public class EnemySpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int index = 0;
         gameObject.transform.position = startPosition.position;
         foreach (GameObject enemy in enemyList)
         {
             Debug.Log("Finding Tile");
-            FindSpawnPointRecursive(enemy, 0);
+            SpawnEnemy(enemy, index);
+            index++;
         }
         Destroy(gameObject);
     }
-    private void FindSpawnPointRecursive(GameObject mobToSpawn, int index)
+    private void SpawnEnemy(GameObject mobToSpawn, int index)
     {
-        for (int search = index; search < maxEnemies; search++)
+        int spawnPoint = GetRandomTile(); //get a random tileID that doesn't match existing entries in tileHasMob[]
+        GameObject newTile = GameObject.Find("TileEnemy" + spawnPoint);
+        Transform spawnPosition = newTile.transform.GetChild(1).transform;
+        Instantiate(mobToSpawn, spawnPosition.position, gameObject.transform.rotation);
+        tileHasMob[index] = spawnPoint; //add the used tileID to tileHasMob[] at the index matching the spawned mob's
+    }
+
+    private int GetRandomTile()
+    {
+        int tileID = Random.Range(1, 9);
+        for (int index = 0; index < tileHasMob.Length - 1; index++) //check all but the last index of tileHasMob[] for matching values
         {
-            int tileNum = Random.Range(1, 2);
-            Debug.Log("Trying Tile " + tileNum);
-            if (tileHasMob[search] == tileNum)
+            if (tileHasMob[index] == tileID)
             {
-                Debug.Log("Has Mob");
-                continue;
-            }
-            else
-            {
-                GameObject newTile = GameObject.Find("TileEnemy" + tileNum);
-                Transform spawnPosition = newTile.transform.GetChild(1).transform;
-                Instantiate(mobToSpawn, spawnPosition.position, gameObject.transform.rotation);
-                if(tileHasMob[search]!= 0)
-                {
-                    
-                }
-                return;
+                tileID = GetRandomTile();   //recursively get a new random number until a non-used value is found
             }
         }
+        return tileID;
     }
 }
