b3b5cd26a37c4707cfa7cbbe054bbca7bf64362f
Aaron Anderson <sylverblade16@gmail.com>
2026-08-07 00:22:50 -0500

    Created EnemyMovement.cs script; copied slime enemy anims from previous project; adjusted slime prefab animators to include any missing animations

---
+++ Assets/Combat System/Scripts/EnemyMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    private void Movement()
+    {
+
+    }
+
+    private void AttackMelee()
+    {
+        
+    }
+
+    private void AttackRanged()
+    {
+        
+    }
+}
